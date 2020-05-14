4ea325150d615385a510e401715c35607c590dfd
Sungmin Oh <fabxoe.kor@gmail.com>
2020-05-14 23:24:54 +0900

    참고:목객체의 기본행동은 Null을 리턴한다. (Optional 타입은 Optional.empty 리턴)
    여기선 목객체를 가지고 (그냥 기본 목객체의 행동에 따라 Optional.empty를 리턴하는게 아니라) 내가 정한 인스턴스를 리턴 하도록 Stubbing했다.

--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
@@ -24,9 +24,13 @@ class StudyServiceTest {
 
     @Test
     void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
-        Optional<Member> optional = memberService.findById(1L);
-//        memberService.validate(2L);//Mock의 리턴 타입이 void인 메서드에 대한 테스트는 아무런 일도 발생하지 않는다.
-//        assertNull(optional);
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("keesun@email.com");
+
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));//목 객체를 Stubbing했다.
+
+
 
         StudyService studyService = new StudyService(memberService, studyRepository);//setter에 사용하거나 필드에 직접 써도 되지만 여기선 생성자에 사용하였다.
         assertNotNull(studyService);
